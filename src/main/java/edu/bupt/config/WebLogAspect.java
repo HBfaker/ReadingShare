5066e6a794f9e939bc2847d9f4ad7746b8efe676
hubiao <736813089@qq.com>
2018-06-23 20:27:14 +0800

    control层的统一日志打印和异常处理（AOP实现）

--- src/main/java/edu/bupt/config/WebLogAspect.java
+++ src/main/java/edu/bupt/config/WebLogAspect.java
@@ -1,8 +1,20 @@
 package edu.bupt.config;
 
+import edu.bupt.exception.ServiceException;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.log4j.Logger;
-import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.*;
 import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.lang.reflect.Method;
+import java.util.Arrays;
 
 /**
  * Created by 73681 on 2018/6/22.
@@ -11,4 +23,30 @@ import org.springframework.stereotype.Component;
 @Component
 public class WebLogAspect {
     private Logger logger = Logger.getLogger(getClass());
+    private static final ThreadLocal<Long> timeTreadLocal = new ThreadLocal<>();
+
+    @Pointcut("execution(public * edu.bupt.controller..*.*(..))")
+    public void webLog(){}
+
+
+    @Before("webLog()")
+    public void before(JoinPoint joinPoint) {
+        // 接收到请求，记录请求内容
+        ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        HttpServletRequest request = attributes.getRequest();
+
+        // 记录下请求内容
+        logger.info("请求地址 : " + request.getRequestURL().toString());
+        logger.info("HTTP_METHOD : " + request.getMethod());
+        logger.info("IP : " + request.getRemoteAddr());
+        logger.info("CLASS_METHOD : " + joinPoint.getSignature().getDeclaringTypeName() + "." + joinPoint.getSignature().getName());
+        logger.info("参数 : " + Arrays.toString(joinPoint.getArgs()));
+    }
+
+    @AfterReturning(returning = "ret", pointcut = "webLog()")
+    public void doAfterReturning(Object ret) throws Throwable {
+        // 处理完请求，返回内容
+        logger.info("RESPONSE : " + ret);
+    }
+
 }
