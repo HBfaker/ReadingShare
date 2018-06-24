047024036e5d89ad6b97796305d20a767b2693cb
Qi <864384653@qq.com>
2018-06-24 09:39:07 +0800

    添加用戶註冊

---
+++ src/main/java/edu/bupt/controller/UserController.java
@@ -0,0 +1,60 @@
+package edu.bupt.controller;
+
+import edu.bupt.mapper.UserMapper;
+import edu.bupt.pojo.User;
+import edu.bupt.serviceImp.UserServiceImp;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/*
+用户
+ */
+@RestController
+public class UserController {
+    /*
+    用户注册
+    http://localhost/register?username=lisi&password=123
+     */
+    @Autowired
+    UserServiceImp userServiceImp;
+    @Autowired
+    UserMapper userMapper;
+    @RequestMapping(value = "/register",method = RequestMethod.GET)
+    public Map register(@RequestParam(name="username",required=false) String username,
+                        @RequestParam(name="password",required=false) String password) throws Exception {
+        Map<String,Object> resp = new HashMap<String,Object>();
+
+        try {
+//        User user = userServiceImp.selectByPrimaryKey(1);
+            if (username == null || password == null) {
+//                throw new Exception("注册时缺少用户名或密码");
+                resp.put("status", 1000);
+                resp.put("msg", "注册时缺少用户名或密码");
+                return resp;
+            }
+            User user = userMapper.selectByUsername(username);
+            if (user != null) {
+                throw new Exception("该用户名已注册");
+            }
+            user = new User();
+            user.setUsername(username);
+            user.setPassword(password);
+
+            userServiceImp.insert(user);
+            System.out.println(user);
+            resp.put("status", 200);
+            resp.put("msg", "ok");
+
+        } catch(Exception e) {
+            e.printStackTrace();
+            throw new Exception(e);
+        }
+        return resp;
+    }
+}
--- src/main/java/edu/bupt/serviceImp/UserServiceImp.java
+++ src/main/java/edu/bupt/serviceImp/UserServiceImp.java
@@ -2,7 +2,7 @@ package edu.bupt.serviceImp;
 
 import edu.bupt.mapper.BaseMapper;
 import edu.bupt.mapper.UserMapper;
-import edu.bupt.pojo.UserOAuth;
+import edu.bupt.pojo.User;
 import edu.bupt.service.UserService;
 import edu.bupt.service.common.AbstractService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,7 +12,7 @@ import org.springframework.stereotype.Service;
  * Created by 73681 on 2018/5/30.
  */
 @Service
-public class UserServiceImp extends AbstractService<UserOAuth,Integer> implements UserService {
+public class UserServiceImp extends AbstractService<User,Integer> implements UserService {
 
     @Autowired
     private UserMapper userMapper;
